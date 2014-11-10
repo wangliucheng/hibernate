a39987e7ecb62e0d8f74bdea8f1eb05e249645c0
wangliucheng <1061942069@qq.com>
2014-11-10 12:15:12 +0800

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,28 @@
+package com.kaishengit.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class HibernateUtil {
+
+	private static SessionFactory factory = buildSessionFactory();
+	
+	private static SessionFactory buildSessionFactory() {
+		Configuration cfg = new Configuration().configure();
+		ServiceRegistry serviceRegistry= new ServiceRegistryBuilder().applySettings(cfg.getProperties()).buildServiceRegistry();
+		return cfg.buildSessionFactory(serviceRegistry);
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		return factory;
+	}
+	
+	public static Session getSession() {
+		return getSessionFactory().getCurrentSession();
+	}
+	
+	
+}
